5c02cad2c51136fd7e28fad390f421ff9ad2596e
Michał Stróżyński <michal.strozynski@o2.pl>
2017-06-23 13:01:01 +0200

    First Commit

---
+++ src/main/java/michu/spring/tutorials/SpringTutorialBuildRestApplication.java
@@ -0,0 +1,12 @@
+package michu.spring.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialBuildRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialBuildRestApplication.class, args);
+	}
+}
